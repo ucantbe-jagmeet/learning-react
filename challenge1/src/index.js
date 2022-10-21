38cc8b04009713951b36c032e64feec50baac69d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-10-21 20:20:10 +0530

    Creating netflix movies list from scratch

---
+++ challenge1/src/index.js
@@ -0,0 +1,15 @@
+var React = require('react'); 
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <> 
+        <h1>Hello world</h1>
+        <p>List of 5 Best Series</p>
+        <ol>
+            <li>Dark</li>
+            <li>Extra curricular</li>
+            <li>My Holo Love</li>
+            <li>My First-2 Love</li>
+            <li>Mr robot</li>
+        </ol>
+    </>, document.getElementById('root'));

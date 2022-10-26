45de84c753fc200e7487f26c0ab67dc477566dc8
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-10-26 20:20:10 +0530

    CSS styling

--- importingCSS/src/index.js
+++ importingCSS/src/index.js
@@ -3,6 +3,6 @@ import  ReactDOM  from "react-dom";
 
 ReactDOM.render(
     <>
-        <h1>CSS Styling</h1>
+        <h1>CSS Styling and properties</h1>
     </>
 ,document.getElementById('root'));

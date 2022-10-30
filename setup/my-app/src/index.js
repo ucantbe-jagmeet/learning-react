b38c7b1e809d99d358b28c3954ba35a6ef35c5bb
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-10-30 20:10:10 +0530

    Hello world in js and react

--- setup/my-app/src/index.js
+++ setup/my-app/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//by react
+// ReactDOM.render('what to show ', 'where to show ', 'callback func');
+ReactDOM.render( <h1>HELLO wOrld...(by react)!</h1>, document.getElementById('root') );
+
+
+//by javaSCript
+var h1 = document.createElement('h1');
+h1.innerHTML= "HELLO WORLD...(by javascript)";
+document.getElementById("root").appendChild(h1);

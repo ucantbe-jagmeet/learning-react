f78dd2fb569e6263ddc975347c8ba974d3efa477
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-01 23:13:09 +0530

    changes

--- challenge2/src/index.js
+++ challenge2/src/index.js
@@ -10,5 +10,6 @@ ReactDOM.render(
     <h1>{`My name is ${fname} ${lname}`}</h1>
     <p>{`Current date is  ${d.getFullYear()}/${d.getMonth()}/${d.getDay()}`}</p>
     <p>{`Current Time is  ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`}</p>
+    
 </>
 ,document.getElementById('root'));

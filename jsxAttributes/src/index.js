6decd5ce3626401042ec9bb816cd933661aa629f
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-02 00:17:46 +0530

    Using jsx Attributes

---
+++ jsxAttributes/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+
+const img1 = "https://picsum.photos/250/300";
+const img2 = "https://picsum.photos/150/300";
+const img3 = "https://picsum.photos/300/300";
+const link = "https://www.youtube.com/";
+
+ReactDOM.render( 
+<>
+    <h1 contentEditable="false"  >JSX ATTRIBUTES</h1>
+    <img src={img2} alt="Random images"/> 
+    <img src={img1} alt="Random images"/> 
+
+    <a href={link} target="_black" > 
+        <img src={img3} alt="Random images"/> 
+    </a>
+
+</>
+,document.getElementById('root'));

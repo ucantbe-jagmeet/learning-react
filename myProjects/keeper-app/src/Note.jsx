9222d615626b0e8f01a17db74b82aa1a4b3a3a6e
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-07 15:19:30 +0530

    keeper-app

---
+++ myProjects/keeper-app/src/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+
+function Note()
+{
+    return (
+        <div className="note">
+            <h1>This is Note Title</h1>
+            <p>This is Note content</p>
+        </div>
+    );
+
+}
+
+export default Note;

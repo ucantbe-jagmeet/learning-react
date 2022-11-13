92999d8dc5c863a2d989633d10f61d4f13beb2a5
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-11-13 12:50:36 +0530

    login Form

---
+++ reactLoginForm/src/App.jsx
@@ -0,0 +1,118 @@
+import React, { useState }  from "react";
+
+
+const App =() =>
+{
+   
+    const [fullName, setFullName] = useState({
+        fname : '',
+        lname : '',
+        email : '',
+        phone : ''
+    });
+
+
+    const inputEvent = (event) =>
+    {
+        console.log(event.target.name);
+        console.log(event.target.value);
+
+        const {value , name } = event.target; // object destructoring
+
+        setFullName((preValue) => {
+            // console.log(preValue);
+
+            if(name === 'fname')
+            {
+                return{
+                    fname : value,
+                    lname : preValue.lname,
+                    email : preValue.email,
+                    phone : preValue.phone
+                };
+            }
+            else if(name === 'lname')
+            {
+                return{
+                    fname : preValue.fname,
+                    lname : value,
+                    email : preValue.email,
+                    phone : preValue.phone
+                };
+            }
+            else if(name === 'email')
+            {
+                return{
+                    fname : preValue.fname,
+                    lname : preValue.lname,
+                    email : value ,
+                    phone : preValue.phone
+                };
+            }
+            else if(name === 'phone')
+            {
+                return{
+                    fname : preValue.fname,
+                    lname : preValue.lnmae,
+                    email : preValue.email,
+                    phone :  value
+                };
+            }
+        })
+
+    };
+    
+    const onSubmits = (e) =>
+    {
+        e.preventDefault();
+        alert('Form Submitted');
+    };
+
+    return (
+    <>
+       <form onSubmit={onSubmits}>
+        <div className="container" >
+                <h1 className="header">Hello {fullName.fname} {fullName.lname}</h1>
+                <p>{fullName.email}</p>
+                <p>{fullName.phone}</p>
+                <input 
+                    className="content" 
+                    type='text' 
+                    placeholder='Enter Your First Name'
+                    name = 'fname'
+                    onChange={inputEvent}
+                    value={fullName.fname}                    
+                />
+                <input 
+                    className="content" 
+                    type='text' 
+                    placeholder='Enter Your Second Name'
+                    name = 'lname'
+                    onChange={inputEvent}
+                    value={fullName.lname}
+                />
+                <input 
+                    className="content" 
+                    type='email' 
+                    placeholder='Enter Your Email'
+                    name = 'email'
+                    onChange={inputEvent}
+                    value={fullName.email}
+                    autoComplete="off"
+                />
+                <input 
+                    className="content" 
+                    type='number' 
+                    placeholder='Enter Your Phone Number'
+                    name = 'phone'
+                    onChange={inputEvent}
+                    value={fullName.phone}
+                />
+                <button type="submit" className="btn" >Submit Me 👍</button>
+            </div>
+       </form>
+    </>
+    );
+}
+
+export default App;

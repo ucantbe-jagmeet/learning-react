03fbd826c1ee5b1c7d3b1d2da3c3ae492cfc6a3f
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-12-01 20:20:10 +0530

    todo-app in adding values

--- myProjects/todo-list/src/App.jsx
+++ myProjects/todo-list/src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React,{ useState} from "react";
+
 
 const App = () =>
 {
+
+    const [inputList , setInputList] = useState("buy apple");
+    const [Items, setItems] = useState([]);
+    const itemEvent = (e) => 
+    {
+        setInputList(e.target.value);
+    };
+
+    const listOfItems = () => 
+    {
+        setItems( (oldItems) => {
+            return [...oldItems , inputList ];
+        })
+    }
 
     return ( 
         <>
@@ -10,10 +25,13 @@ const App = () =>
                     <br/>
                     <h1>ToDo List</h1>
                     <br/>
-                    <input type='text' placeholder="Add Items" />
-                    <button> + </button>
+                    <input type='text' placeholder="Add Items" onChange={itemEvent}/>        
+                    <button onClick={listOfItems} > + </button>
                     <ol>
-                        <li> buy Apple</li>
+                        {Items.map( (itemval) => {
+                            return <li>{itemval}</li>;
+                        })}
+
                     </ol>
                 </div>
             </div>

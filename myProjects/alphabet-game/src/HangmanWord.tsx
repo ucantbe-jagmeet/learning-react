8ccee4183f1716decc57bb0a4bb281eeb37a0997
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2022-12-05 22:37:44 +0530

    making hangman word and hangman word

--- myProjects/alphabet-game/src/HangmanWord.tsx
+++ myProjects/alphabet-game/src/HangmanWord.tsx
@@ -1,1 +1,11 @@
-export function HangmanWord(){}
+export function HangmanWord(){
+    const word = "test";
+    const guessedLetters = ["t"]
+    return <div style={{ display:"flex", gap:".25em", fontSize:"5rem", fontWeight:"bold", textTransform: "uppercase",fontFamily:"monospace" }}>
+        {word.split("").map((letter, index) => (
+            <span style={{ borderBottom:" .1em solid black"}} >
+                <span style={{visibility: guessedLetters.includes(letter) ? "visible": "hidden" }} key={index} >{letter}</span>
+            </span>
+        ))}
+    </div>
+}
--- myProjects/alphabet-game/src/Keyboard.tsx
+++ myProjects/alphabet-game/src/Keyboard.tsx
@@ -1,1 +1,3 @@
-export function Keyboard(){}
+export function Keyboard(){
+    return <></>
+}

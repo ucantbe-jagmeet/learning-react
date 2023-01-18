b680187f693cfd8b395d5dae8740344045ef1c9c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-01-18 08:16:07 +0530

    learning useContext hook

---
+++ context-api/src/CompC.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FirstName, LastName } from "./App";
+
+const CompC = () => {
+  return (
+    <>
+      <FirstName.Consumer>
+        {(fname) => {
+          return (
+            <LastName.Consumer>
+              {(lname) => {
+                return (
+                  <h1>
+                    This is component C and my name is {fname} {lname}
+                  </h1>
+                );
+              }}
+            </LastName.Consumer>
+          );
+        }}
+      </FirstName.Consumer>
+    </>
+  );
+};
+
+export default CompC;
---
+++ use-context/CompA.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompA = () => {
+  return <div></div>;
+};
+
+export default CompA;

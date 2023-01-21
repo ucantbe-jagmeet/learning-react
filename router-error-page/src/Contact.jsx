58b73b1abddd42ca9ea783693d1891e8166f4933
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-01-21 10:12:22 +0530

    creating error page

---
+++ router-error-page/src/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>Contact Page</h1>
+      </section>
+    </>
+  );
+};
+
+export default Contact;

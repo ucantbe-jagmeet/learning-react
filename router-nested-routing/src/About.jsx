0d65bb53d4b9d5fba7f311eb4ee7ba029fbb516b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-01-23 06:35:40 +0530

    learning nesting routing

---
+++ router-nested-routing/src/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+
+const About = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <h1>About Page</h1>
+      </section>
+    </>
+  );
+};
+
+export default About;

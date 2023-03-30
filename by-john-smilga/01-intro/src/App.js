2468c1f41c8394ad314280f013fde05203b82b57
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-30 21:59:18 +0530

    ad

--- by-john-smilga/01-intro/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="">
-      <header className="">hello</header>
-    </div>
-  );
-}
-
-export default App;
--- by-john-smilga/02-first-component/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-function Greet() {
-  return <h2> My First Component</h2>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greet />);

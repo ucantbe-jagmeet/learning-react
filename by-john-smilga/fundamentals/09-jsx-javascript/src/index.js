1fa769ee8cdee9a6808f98bc04cf00383c8f38d5
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-31 13:14:00 +0530

    add

--- by-john-smilga/fundamentals/09-jsx-javascript/src/index.js
+++ by-john-smilga/fundamentals/09-jsx-javascript/src/index.js
@@ -14,34 +14,25 @@ const BookList = () => {
 };
 
 const Book = () => {
-  return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://images-eu.ssl-images-amazon.com/images/I/61yB0UFlM3L._AC_UL600_SR600,400_.jpg"
-      alt="image"
-    />
-  );
-};
-const Title = () => {
   const inlineCss = {
     color: "#617d98",
     fontSize: "0.75rem",
     marginTop: "0.5rem",
   };
-  return <h2 style={inlineCss}> Grandma's Bag of Stories</h2>;
-};
-
-const Author = () => {
-  return <h3 style={{ color: "#617d98", fontSize: "0.75rem" }}>Sudha Murty</h3>;
+  const title = `Grandma's Bag of Stories`;
+  const author = `Sudha Murty`;
+  return (
+    <article className="book">
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/I/61yB0UFlM3L._AC_UL600_SR600,400_.jpg"
+        alt="image"
+      />
+      <h2 style={inlineCss}> ${title}</h2>
+      <h3 style={{ color: "#617d98", fontSize: "0.75rem" }}>
+        ${author.toUpperCase()}
+      </h3>
+    </article>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

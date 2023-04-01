ac1179a28a6105f5129aa1f7b260a3826af48a71
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-01 13:33:44 +0530

    add

---
+++ by-john-smilga/fundamentals/24-es6/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    id: 1,
+    title: `Grandma's Bag of Stories`,
+    author: `Sudha Murty`,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/61yB0UFlM3L._AC_UL600_SR600,400_.jpg",
+  },
+  {
+    id: 2,
+    title: `Rich dad poor dad`,
+    author: `Robert Kiyosaki`,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81BE7eeKzAL._AC_UL600_SR600,400_.jpg",
+  },
+];

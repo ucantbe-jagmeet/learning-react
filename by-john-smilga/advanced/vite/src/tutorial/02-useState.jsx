d01dd36e9748974222bbac153c7c1a531a90c47a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-05 20:20:10 +0530

    useState count btn

--- by-john-smilga/advanced/vite/src/tutorial/02-useState.jsx
+++ by-john-smilga/advanced/vite/src/tutorial/02-useState.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import { useState } from "react";
 
 const useStateBasics = () => {
+  const [Count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(Count + 1);
+    // setCount("Pants");
+  };
+
   return (
     <div>
-      <h1>Use State hook</h1>
+      <h4>You Clicked {Count} Times</h4>
+      <button type="button" className="btn" onClick={handleClick}>
+        {" "}
+        click Me
+      </button>
     </div>
   );
 };

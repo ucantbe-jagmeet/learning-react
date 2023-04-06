768e46bcb31bc308d2929624f7ec1e7d1a9acaf4
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-06 20:20:10 +0530

    useState challenge

--- by-john-smilga/advanced/vite/src/tutorial/02-useState.jsx
+++ by-john-smilga/advanced/vite/src/tutorial/02-useState.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { useState } from "react";
+import data from "../data.js";
 
 const useStateBasics = () => {
-  const [Count, setCount] = useState(0);
+  // const [Count, setCount] = useState(0);
+  const [people, setPeople] = useState(data);
+
   const handleClick = () => {
-    setCount(Count + 1);
-    // setCount("Pants");
+    console.log(data);
   };
 
   return (
     <div>
-      <h4>You Clicked {Count} Times</h4>
-      <button type="button" className="btn" onClick={handleClick}>
-        {" "}
-        click Me
-      </button>
+      {people.map((items) => {
+        const { id, name } = items;
+
+        return (
+          <>
+            <div key={id}>
+              <h4>{name}</h4>
+              <button type="button" onClick={handleClick}>
+                Remove
+              </button>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 };

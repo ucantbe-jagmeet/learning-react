5dd3d38721f8241f131b8ed123f459fb1784e7e0
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-13 15:48:51 +0530

    review

--- by-john-smilga/advanced/projects/03-reviews/src/App.jsx
+++ by-john-smilga/advanced/projects/03-reviews/src/App.jsx
@@ -1,17 +1,70 @@
 import people from "./data.js";
 import { useState } from "react";
-import { FaBeer } from "react-icons/fa";
+import {
+  FaChevronCircleLeft,
+  FaChevronCircleRight,
+  FaQuoteRight,
+} from "react-icons/fa";
 
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  console.log(name);
+  const checkNum = (num) => {
+    if (num > people.length - 1) {
+      return 0;
+    }
+    if (num < 0) {
+      return people.length - 1;
+    }
+    return num;
+  };
+
+  const nextPerson = () => {
+    setIndex((curr) => {
+      const newIndex = curr + 1;
+      return checkNum(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((curr) => {
+      const newIndex = curr - 1;
+      return checkNum(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    console.log(Math.floor(Math.random() * people.length));
+
+    setIndex(Math.floor(Math.random() * people.length));
+  };
+
   return (
-    <h2>
-      Reviews Starter
-      <FaBeer className="beer" />
-    </h2>
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronCircleLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronCircleRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          Surprise me
+        </button>
+      </article>
+    </main>
   );
 };
 export default App;

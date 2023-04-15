8d12100dcd01873aeb677f8fb1367ed686b70c47
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-15 12:20:14 +0530

    menu project

---
+++ by-john-smilga/advanced/projects/05-menu/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data.js";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ["all", ...tempSet];
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+// console.log(allCategories);
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    // console.log(category);
+
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  // console.log(categories);
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
+export default App;

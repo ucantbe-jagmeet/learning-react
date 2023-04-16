a04c003c431b70e6a5fbc857ff5d881a1b158830
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-16 21:38:55 +0530

    slider project

--- by-john-smilga/advanced/projects/07-slider/src/Carousel.jsx
+++ by-john-smilga/advanced/projects/07-slider/src/Carousel.jsx
@@ -1,21 +1,50 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { shortList, list, longList } from "./data.js";
-import { useState } from "react";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 
 const Carousel = () => {
-  const [people, setPeople] = useState(shortList);
+  const [people, setPeople] = useState(longList);
+  const [currPerson, setCurrPerson] = useState(0);
 
-  const prevSlide = () => {};
-  const nextSlide = () => {};
+  const prevSlide = () => {
+    setCurrPerson((oldPerson) => {
+      const result = (oldPerson - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  const nextSlide = () => {
+    setCurrPerson((oldPerson) => {
+      const result = (oldPerson + 1) % people.length;
+      return result;
+    });
+  };
+
+  useEffect(() => {
+    let sliderId = setInterval(() => {
+      nextSlide();
+    }, 2000);
+
+    return () => {
+      clearInterval(sliderId);
+    };
+  }, [currPerson]);
 
   return (
     <section className="slider-container">
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, title, quote } = person;
         return (
-          <article className="slide" key={id}>
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${100 * (personIndex - currPerson)}%)`,
+              opacity: personIndex === currPerson ? 1 : 0,
+              visibility: personIndex === currPerson ? "visible" : "hidden",
+            }}
+            key={id}
+          >
             <img src={image} alt={name} className="person-img" />
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>

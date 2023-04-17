4bc2dcb41b0548ad02d8316159318dc948f8ed56
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-17 19:00:29 +0530

    project structure

--- by-john-smilga/advanced/vite/src/App.jsx
+++ by-john-smilga/advanced/vite/src/App.jsx
@@ -1,9 +1,10 @@
 import Starter from "./tutorial/03-conditional-rendering/09-useEffect-events";
+import Navbar from "./tutorial/04-project-structure/navbar";
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      <Navbar />
     </div>
   );
 }
---
+++ by-john-smilga/advanced/vite/src/tutorial/04-project-structure/navbar/index.jsx
@@ -0,0 +1,9 @@
+// import React from "react";
+
+// const Navbar = () => {
+//   return <h2>Navbar from index jsx</h2>;
+// };
+
+// export default Navbar;
+
+export { default } from "./Navbar";

1815310d76db317090b487dc45ad2ab50bd6dc0d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-18 21:00:44 +0530

    useRef initial render

--- by-john-smilga/advanced/vite/src/tutorial/07-useRef/01-useRef-basics.jsx
+++ by-john-smilga/advanced/vite/src/tutorial/07-useRef/01-useRef-basics.jsx
@@ -3,11 +3,20 @@ import { useEffect, useRef, useState } from "react";
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
   const refContainer = useRef(null);
+  const isMounted = useRef(false);
+
   // console.log(refContainer);
 
-  // useEffect(() => {
-  //   console.log(refContainer);
-  // });
+  useEffect(() => {
+    refContainer.current.focus();
+  });
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+    console.log("re-render");
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();

c3ef37772daa92922681530ba26b65aea3ed4dfd
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-19 15:35:27 +0530

    modal done

---
+++ by-john-smilga/advanced/projects/11-modal/src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useGlobalContext } from "./Context";
+import { FaBars } from "react-icons/fa";
+
+const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        Show Modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;

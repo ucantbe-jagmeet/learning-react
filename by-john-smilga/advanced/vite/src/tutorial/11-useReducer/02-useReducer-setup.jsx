f1581f0a403a5b8a6336143623ed093b2b55a934
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-20 12:54:36 +0530

    adding functionality using useREducer

--- by-john-smilga/advanced/vite/src/tutorial/11-useReducer/02-useReducer-setup.jsx
+++ by-john-smilga/advanced/vite/src/tutorial/11-useReducer/02-useReducer-setup.jsx
@@ -6,29 +6,45 @@ const defaultState = {
   isLoading: false,
 };
 
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
 const reducer = (state, action) => {
-  if (action.type === "CLEAR_LIST") {
+  if (action.type === CLEAR_LIST) {
     return { ...state, people: [] };
   }
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+  if (action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id
+    );
+    return { people: newPeople };
+  }
+  // return state;
+  throw new Error(`No matching "${action.type}"- action type`);
 };
 
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
     // let newPeople = people.filter((person) => person.id !== id);
     // setPeople(newPeople);
   };
 
   const clearList = () => {
-    dispatch({ type: "CLEAR_LIST" });
+    dispatch({ type: CLEAR_LIST });
     // setPeople([])
   };
   const resetList = () => {
+    dispatch({ type: RESET_LIST });
     // setPeople(data)
   };
 
-  console.log(state);
   return (
     <div>
       {state.people.map((person) => {

d551cdbcbabd1c648255c1e4d01cd84828e00a8d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-21 21:33:29 +0530

    axios get request

--- by-john-smilga/axios/src/App.js
+++ by-john-smilga/axios/src/App.js
@@ -1,8 +1,10 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Starter from "./examples/1-first-request";
 function App() {
   return (
     <main>
       <Title />
+      <Starter />
     </main>
   );
 }

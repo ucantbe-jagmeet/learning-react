d9458a39f87de52d7dfb78697f881768b2d69e1d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-22 16:11:26 +0530

    Protected Route

--- by-john-smilga/react-router-v6/src/App.js
+++ by-john-smilga/react-router-v6/src/App.js
@@ -8,6 +8,7 @@ import SharedLayout from "./pages/SharedLayout";
 import SingleProduct from "./pages/SingleProduct";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -20,7 +21,14 @@ function App() {
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
           <Route path="login" element={<Login setUser={setUser} />} />
-          <Route path="dashboard" element={<Dashboard user={user} />} />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user} />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>

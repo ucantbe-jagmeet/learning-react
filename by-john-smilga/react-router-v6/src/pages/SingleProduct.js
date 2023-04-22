4cb8b8a1cdc8eed609d9e0fc2d739949939bbb34
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-22 15:19:48 +0530

    single product page

--- by-john-smilga/react-router-v6/src/pages/SingleProduct.js
+++ by-john-smilga/react-router-v6/src/pages/SingleProduct.js
@@ -1,9 +1,14 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../data";
+
 const SingleProduct = () => {
   const { productId } = useParams();
+  const product = products.find((product) => product.id === productId);
+  const { image, name } = product;
   return (
     <section className="section product">
-      <h2>{productId}</h2>
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
       <Link to="/products" className="btn">
         Back to products
       </Link>

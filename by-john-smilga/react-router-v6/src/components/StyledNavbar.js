4656022bb037f3d6ec98019ea9d66b34cd90efe3
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-22 14:12:53 +0530

    NavLink

---
+++ by-john-smilga/react-router-v6/src/components/StyledNavbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink
+        to="/"
+        // style={({ isActive }) => {
+        //   return { color: isActive ? "red" : "grey" };
+        // }}
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/products"
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Products
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;

ff48229527f33b5327e383358cff2854bc47c0db
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-22 14:00:17 +0530

    index pages

--- by-john-smilga/react-router-v6/src/pages/Home.js
+++ by-john-smilga/react-router-v6/src/pages/Home.js
@@ -1,11 +1,8 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <>
-      <Navbar />
       <section className="section">
-        <Outlet />
+        <h2>Home Page</h2>
       </section>
     </>
   );

dc976df72cc3debf6ff70b9c8ef130211fc79f44
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-22 13:19:23 +0530

    page componenets

--- by-john-smilga/react-router-v6/src/App.js
+++ by-john-smilga/react-router-v6/src/App.js
@@ -1,18 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route
-          path="testing"
-          element={
-            <div>
-              <h2>Testing Page</h2>
-            </div>
-          }
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );

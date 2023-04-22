76b108ea6eb0d4de8d322449c3fbf0add65d3bfd
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-22 13:26:08 +0530

    setting up Error page

--- by-john-smilga/react-router-v6/src/pages/Error.js
+++ by-john-smilga/react-router-v6/src/pages/Error.js
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <section className='section'>
-      <h2>Error</h2>
+    <section className="section">
+      <h2>404</h2>
+      <p>Page not found</p>
+      <Link to="/" className="btn">
+        Back home
+      </Link>
     </section>
   );
 };

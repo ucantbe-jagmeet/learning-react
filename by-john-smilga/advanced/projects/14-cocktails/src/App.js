6b58d6b17770969dbf3c39fdff7027f258c75316
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-24 10:23:49 +0530

    setup

--- by-john-smilga/advanced/projects/14-cocktails/src/App.js
+++ by-john-smilga/advanced/projects/14-cocktails/src/App.js
@@ -1,18 +1,26 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import pages
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 // import components
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-      <h2>app component</h2>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="cocktail" element={<SingleCocktail />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

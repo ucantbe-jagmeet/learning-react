0370a499ab3e3661fc243f71a76daade99ffc329
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-27 00:36:29 +0530

    clear cart functionality

--- by-john-smilga/advanced/projects/13-cart/src/CartContainer.jsx
+++ by-john-smilga/advanced/projects/13-cart/src/CartContainer.jsx
@@ -3,7 +3,7 @@ import cartItems from "./data";
 import { useGlobalContext } from "./context";
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext();
+  const { cart, clearCart } = useGlobalContext();
   const cartArray = Array.from(cart.entries());
 
   if (cartArray.length === 0) {
@@ -38,10 +38,7 @@ const CartContainer = () => {
             total <span>$10</span>
           </h5>
         </div>
-        <button
-          className="btn btn-hipster"
-          onClick={() => console.log("clear cart")}
-        >
+        <button className="btn btn-hipster" onClick={clearCart}>
           clear cart
         </button>
       </footer>

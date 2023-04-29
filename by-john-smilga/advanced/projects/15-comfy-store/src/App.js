0c8932db1df19b98160566ce5ff2547859f95870
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-04-29 16:08:41 +0530

    auth Wrapper

--- by-john-smilga/advanced/projects/15-comfy-store/src/App.js
+++ by-john-smilga/advanced/projects/15-comfy-store/src/App.js
@@ -10,38 +10,40 @@ import {
   Cart,
   Error,
   Checkout,
-  Private,
+  PrivateRoute,
+  AuthWrapper,
 } from "./pages";
-import PrivateRoute from "./pages/PrivateRoute";
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
-        <Route exact path="/products">
-          <Products />
-        </Route>
-        <Route exact path="/products/:id" children={<SingleProduct />} />
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
+          <Route exact path="/products/:id" children={<SingleProduct />} />
 
-        <PrivateRoute exact path="/checkout">
-          <Checkout />
-        </PrivateRoute>
-        <Route exact path="*">
-          <Error />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+          <PrivateRoute exact path="/checkout">
+            <Checkout />
+          </PrivateRoute>
+          <Route exact path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
--- by-john-smilga/advanced/projects/15-comfy-store/src/components/CartButtons.js
+++ by-john-smilga/advanced/projects/15-comfy-store/src/components/CartButtons.js
@@ -17,7 +17,7 @@ const CartButtons = () => {
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
       {myUser ? (

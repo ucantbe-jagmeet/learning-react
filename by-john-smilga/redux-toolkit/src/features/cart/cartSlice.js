41e0fba3846eb61ea1830dc6411f1aa92c997718
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-04 01:46:32 +0530

    createAsyncThunk more options

--- by-john-smilga/redux-toolkit/src/features/cart/cartSlice.js
+++ by-john-smilga/redux-toolkit/src/features/cart/cartSlice.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+// import { openModal } from "../modal/modalSlice";
 
-const url = "https://course-api.com/react-useReducer-cart-project";
+const url = "https://course-api.com/react-useReducer-cart-projects";
 
 const initialState = {
   cartItems: [],
@@ -9,11 +11,22 @@ const initialState = {
   isLoading: true,
 };
 
-export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
-  return fetch(url)
-    .then((resp) => resp.json())
-    .catch((err) => console.log(err));
-});
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (name, thunkAPI) => {
+    try {
+      // console.log(name);
+      // console.log(thunkAPI);
+      // console.log(thunkAPI.getState());
+      // thunkAPI.dispatch(openModal());
+
+      const resp = await axios(url);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Something went wrong");
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -52,11 +65,11 @@ const cartSlice = createSlice({
       state.isLoading = true;
     },
     [getCartItems.fulfilled]: (state, action) => {
-      console.log(action);
       state.isLoading = false;
       state.cartItems = action.payload;
     },
-    [getCartItems.rejected]: (state) => {
+    [getCartItems.rejected]: (state, action) => {
+      console.log(action);
       state.isLoading = false;
     },
   },

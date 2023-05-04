c70b0b395145ef4dc9b1ea564267f746b0ba39bb
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-04 20:39:13 +0530

    landing page structure

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/pages/Landing.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/pages/Landing.js
@@ -2,7 +2,26 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h1>Landing</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+            hic omnis fuga excepturi ipsum fugit modi aliquam ullam nihil ut.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;

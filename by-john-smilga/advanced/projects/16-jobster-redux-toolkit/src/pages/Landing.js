42a9b3eaeb2376aee2b0ca4d43caa30e18ea40ee
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-04 22:27:25 +0530

    logo component

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/pages/Landing.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/pages/Landing.js
@@ -1,12 +1,13 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 import styled from "styled-components";
+import Logo from "../components/Logo";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobster logo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">

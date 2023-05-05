e8fb08e8d795309582dbe1033317f13039f55e07
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-06 05:04:24 +0530

    Clear Values Reducer

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/features/job/jobSlice.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/features/job/jobSlice.js
@@ -23,9 +23,15 @@ const jobSlice = createSlice({
     handleChange: (state, { payload: { name, value } }) => {
       state[name] = value;
     },
+    clearValues: () => {
+      return {
+        ...initialState,
+      };
+      return initialState;
+    },
   },
 });
 
-export const { handleChange } = jobSlice.actions;
+export const { handleChange, clearValues } = jobSlice.actions;
 
 export default jobSlice.reducer;

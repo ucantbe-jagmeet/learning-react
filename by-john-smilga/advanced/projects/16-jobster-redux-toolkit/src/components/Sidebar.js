fcc815ab1579af0048fb48251e67527ba472250c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-06 01:57:17 +0530

    sidebar nav links

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/components/Sidebar.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/components/Sidebar.js
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../features/user/userSlice";
+import links from "../utils/links";
 
 const Sidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
@@ -29,7 +30,25 @@ const Sidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">nav links</div>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, id, icon } = link;
+
+              return (
+                <NavLink
+                  to={path}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link"
+                  }
+                  key={id}
+                  onClick={toggle}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Wrapper>

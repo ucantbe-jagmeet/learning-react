bdc44ff5c8a635ddb32002e9992ed929d9df2914
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-05 22:47:25 +0530

    navigate to Dashboard

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/pages/Register.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/pages/Register.js
@@ -4,6 +4,7 @@ import Wrapper from "../assets/wrappers/RegisterPage";
 import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser, registerUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const initialState = {
   name: "",
@@ -17,6 +18,7 @@ const Register = () => {
   const { isLoading, user } = useSelector((store) => store.user);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -43,6 +45,14 @@ const Register = () => {
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  }, [user]);
 
   return (
     <Wrapper className="full-page">

61e153b26474c6efc134f6d8486b439f47212baa
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-06 00:56:18 +0530

    Navbar Structure

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/components/Navbar.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/components/Navbar.js
@@ -1,11 +1,49 @@
 import Wrapper from "../assets/wrappers/Navbar";
-import { FaHome } from "react-icons/fa";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import Logo from "./Logo";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Navbar = () => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome />
+      <div className="nav-center">
+        <button
+          className="toggle-btn"
+          type="button"
+          onClick={() => console.log("toggle button")}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className="logo-text">dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button
+            type="button"
+            className="btn"
+            onClick={() => console.log("toggle logout-dropdown")}
+          >
+            <FaUserCircle />
+            {user?.name}
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => {
+                console.log("logout user");
+              }}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };

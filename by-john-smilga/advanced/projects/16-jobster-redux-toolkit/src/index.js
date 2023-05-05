2e8f764d564a56ca1f994d02bdf1e15c7801f469
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-05 19:35:27 +0530

    User Slice Setup

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/index.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/index.js
@@ -3,7 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "normalize.css";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>
+);

c459a4d1efb8ee4a6c9fd444eb637037dcc09749
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-06 19:15:52 +0530

    job component structure

--- by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/features/allJobs/allJobsSlice.js
+++ by-john-smilga/advanced/projects/16-jobster-redux-toolkit/src/features/allJobs/allJobsSlice.js
@@ -30,7 +30,6 @@ export const getAllJobs = createAsyncThunk(
           authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
         },
       });
-      console.log(resp.data);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue("There was an error");

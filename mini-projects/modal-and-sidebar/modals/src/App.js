00af05f2dadda5c68de156e959f763789c5c1a22
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-20 23:30:02 +0530

    icon design complete

--- mini-projects/modal-and-sidebar/modals/src/App.js
+++ mini-projects/modal-and-sidebar/modals/src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { TbBrandCpp, DiJava, GoGraph } from "./icons";
 
 function App() {
   return (
     <main className="h-screen bg-slate-200">
-      <h1>Hello World</h1>
+      <div className="text-white bg-purple-300 w-fit">
+        <p className=" px-2 py-4 text-3xl text-purple-600">
+          <GoGraph />
+        </p>
+      </div>
+      <h1 className="text-3xl  bg-purple-300 px-2 py-1 border  rounded-tr-lg rounded-br-lg w-fit text-white">
+        <TbBrandCpp />
+      </h1>
     </main>
   );
 }
---
+++ mini-projects/modal-and-sidebar/modals/src/icons/index.js
@@ -0,0 +1,3 @@
+export { GoGraph } from "react-icons/go";
+export { TbBrandCpp } from "react-icons/tb";
+export { DiJava } from "react-icons/di";

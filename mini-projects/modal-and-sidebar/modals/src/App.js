fb7dcbc94d88e52366a64e2a96adec89e4e1bb89
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-21 08:19:52 +0530

    code optiomisation

--- mini-projects/modal-and-sidebar/modals/src/App.js
+++ mini-projects/modal-and-sidebar/modals/src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
 import { useState } from "react";
-import {
-  TbBrandCpp,
-  DiJava,
-  GoGraph,
-  IoLogoJavascript,
-  FaPython,
-  FaRust,
-} from "./icons";
+
 import Title from "./components/Title";
+import Data from "./icons";
+
 function App() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
@@ -25,46 +20,21 @@ function App() {
         onMouseEnter={() => setSidebarOpen(!isSidebarOpen)}
         onMouseLeave={() => setSidebarOpen(!isSidebarOpen)}
       >
-        {/* logo starts */}
-        <p className=" px-2 py-4 text-2xl text-purple-600 flex    border-r-purple-400 border-2 border-t-0 border-l-0 border-b-0 hover:text-blue-400">
-          <GoGraph className="mx-2 text-3xl" />
-          {isSidebarOpen ? <Title text="Languages" /> : null}
-        </p>
-        {/* logo ends */}
+        {Data.map((link) => {
+          const { id, icon, text } = link;
 
-        {/* logo starts */}
-        <p className=" px-2 py-4 text-2xl text-purple-600 flex    border-r-purple-400 border-2 border-t-0 border-l-0 border-b-0 hover:text-blue-400">
-          <TbBrandCpp className="mx-2 text-3xl" />
-          {isSidebarOpen ? <Title text="CPP" /> : null}
-        </p>
-        {/* logo ends */}
-
-        {/* logo starts */}
-        <p className=" px-2 py-4 text-2xl text-purple-600 flex    border-r-purple-400 border-2 border-t-0 border-l-0 border-b-0 hover:text-blue-400">
-          <DiJava className="mx-2 text-3xl" />
-          {isSidebarOpen ? <Title text="Java" /> : null}
-        </p>
-        {/* logo ends */}
-
-        {/* logo starts */}
-        <p className=" px-2 py-4 text-2xl text-purple-600 flex    border-r-purple-400 border-2 border-t-0 border-l-0 border-b-0 hover:text-blue-400 ">
-          <IoLogoJavascript className="mx-2 text-3xl" />
-          {isSidebarOpen ? <Title text="Javascript" /> : null}
-        </p>
-        {/* logo ends */}
-        {/* logo starts */}
-        <p className=" px-2 py-4 text-2xl text-purple-600 flex    border-r-purple-400 border-2 border-t-0 border-l-0 border-b-0 hover:text-blue-400">
-          <FaPython className="mx-2 text-3xl" />
-          {isSidebarOpen ? <Title text="Python" /> : null}
-        </p>
-        {/* logo ends */}
-
-        {/* logo starts */}
-        <p className=" px-2 py-4 text-2xl text-purple-600 flex    border-r-purple-400 border-2 border-t-0 border-l-0 border-b-0 hover:text-blue-400">
-          <FaRust className="mx-2 text-3xl" />
-          {isSidebarOpen ? <Title text="Rust" /> : null}
-        </p>
-        {/* logo ends */}
+          return (
+            <>
+              <p
+                key={id}
+                className=" px-2 py-4 text-2xl text-purple-600 flex    border-r-purple-400 border-2 border-t-0 border-l-0 border-b-0 hover:text-blue-400"
+              >
+                <p className="mx-2 text-3xl">{icon}</p>
+                {isSidebarOpen ? <Title text={text} /> : null}
+              </p>
+            </>
+          );
+        })}
       </div>
     </main>
   );
--- mini-projects/modal-and-sidebar/modals/src/icons/index.js
+++ mini-projects/modal-and-sidebar/modals/src/icons/index.js
@@ -1,6 +1,40 @@
-export { GoGraph } from "react-icons/go";
-export { TbBrandCpp } from "react-icons/tb";
-export { DiJava } from "react-icons/di";
-export { IoLogoJavascript } from "react-icons/io";
-export { FaPython } from "react-icons/fa";
-export { FaRust } from "react-icons/fa";
+import { GoGraph } from "react-icons/go";
+import { TbBrandCpp } from "react-icons/tb";
+import { DiJava } from "react-icons/di";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaPython, FaRust } from "react-icons/fa";
+
+const data = [
+  {
+    id: 1,
+    text: "Logo",
+    icon: <GoGraph />,
+  },
+  {
+    id: 2,
+    text: "CPP",
+    icon: <TbBrandCpp />,
+  },
+  {
+    id: 3,
+    text: "JAVA",
+    icon: <DiJava />,
+  },
+  {
+    id: 4,
+    text: "Javascript",
+    icon: <IoLogoJavascript />,
+  },
+  {
+    id: 5,
+    text: "Python",
+    icon: <FaPython />,
+  },
+  {
+    id: 6,
+    text: "CPP",
+    icon: <FaRust />,
+  },
+];
+
+export default data;

48cdebafd74185100c70dac44805e49c4a7319c6
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-22 17:17:39 +0530

    Reviews section done

--- by-john-smilga-2/01-fundamentals-project/03-reviews/src/App.tsx
+++ by-john-smilga-2/01-fundamentals-project/03-reviews/src/App.tsx
@@ -1,9 +1,57 @@
+import { useState } from "react"
+import people from "./data"
+import { FaChevronLeft , FaChevronRight , FaQuoteRight} from "react-icons/fa"
+
 function App() {
+  const [ index, setIndex] = useState(0)
+  const { name, job, image, text, id } = people[index] 
+
+  console.log( name , job, id, text, image);
+  
+
+  const checkNumber = ( number:number) =>{
+     if( number > people.length -1){
+      return 0 
+     }
+     if( number < 0){
+      return people.length -1
+     }
+     return number
+  }
+
+
+  const nextPerson = () =>{
+    setIndex( (currentIndex)=>{
+      const newIndex = currentIndex + 1
+      return checkNumber(newIndex)
+    })
+  };
+  
+  const prevPerson = () =>{
+    setIndex( (currentIndex)=>{
+      const newIndex = currentIndex - 1
+      return checkNumber(newIndex)
+    })
+  };
 
   return (
-    <>
-      <h2>Reviews Bolte</h2>
-    </>
+    <main>
+      <article className="review">
+        <div className="img-container">
+            <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight/>
+            </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      <div className="btn-container">
+        <button className="prev-btn" onClick={prevPerson}><FaChevronLeft/></button>
+        <button className="next-btn" onClick={nextPerson}><FaChevronRight/></button>
+      </div>
+      </article>
+    </main>
   )
 }
 
--- by-john-smilga-2/01-fundamentals-project/03-reviews/src/data.ts
+++ by-john-smilga-2/01-fundamentals-project/03-reviews/src/data.ts
@@ -17,14 +17,14 @@ const reviews = [
       id: 3,
       name: 'peter jones',
       job: 'intern',
-      image: 'https://course-api.com/images/people/person-4.jpeg',
+      image: 'https://course-api.com/images/people/person-3.jpeg',
       text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
     },
     {
       id: 4,
       name: 'bill anderson',
       job: 'the boss',
-      image: 'https://course-api.com/images/people/person-3.jpeg',
+      image: 'https://course-api.com/images/people/person-4.jpeg',
       text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
     },
   ];

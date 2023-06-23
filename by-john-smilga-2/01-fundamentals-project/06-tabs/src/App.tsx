2a501854518d32d9ff781e1384ff032bc83f4a99
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-23 14:05:40 +0530

    fetching tabs

--- by-john-smilga-2/01-fundamentals-project/06-tabs/src/App.tsx
+++ by-john-smilga-2/01-fundamentals-project/06-tabs/src/App.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react"
+
+const url = "https://course-api.com/react-tabs-project"
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [jobs, setJobs] = useState([])
+
+  const fetchJobs = async () =>{
+    setIsLoading(true)
+    try {
+    const response = await fetch( url)
+    const newjobs = await response.json()
+
+    setJobs(newjobs)
+    setIsLoading(false)
+   } catch (error) {
+    console.log(error);
+   }
+  }
+
+  useEffect(()=>{
+    fetchJobs()
+  },[])
+
+  if(isLoading){
+    return <section className="jobs-center">
+      <div className="loading"></div>
+    </section>
+  }
 
   return (
     <>

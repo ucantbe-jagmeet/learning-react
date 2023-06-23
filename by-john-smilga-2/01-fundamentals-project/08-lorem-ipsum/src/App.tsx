c53f27e793258fc96301f4f785404a4b97bc28e1
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-23 22:01:06 +0530

    lorem ipsum generator done

--- by-john-smilga-2/01-fundamentals-project/08-lorem-ipsum/src/App.tsx
+++ by-john-smilga-2/01-fundamentals-project/08-lorem-ipsum/src/App.tsx
@@ -2,15 +2,30 @@ import { useState} from 'react'
 import data from './data'
 
 function App() {
-  const [ count, setCount] = useState(1)
+  const [ count, setCount] = useState<number>(1)
+  const [ text, setText] = useState<string[]>([])
+
+  const handleSubmit = (e: { preventDefault: () => void }) =>{
+    e.preventDefault()
+    setText( data.slice(0, count))    
+  }
+
+
   return (
     <section className='section-center'>
         <h4>Tired of boring Lorem ipsum ?</h4>
-        <form className='lorem-form'>
+        <form className='lorem-form' onSubmit={handleSubmit}>
           <label htmlFor="amount">paragraphs:</label>
           <input type="number" name="amount" id="amount" min='1' step='1' max='8' value={count} onChange={(e)=> setCount(Number(e.target.value))}/>
           <button className='btn' type='submit'>generate</button>
         </form>
+        <article className='lorem-text'>
+          {
+            text.map(( item, index)=>{
+              return <p key={index}>{item}</p>
+            })
+          }
+        </article>
     </section>
   )
 }

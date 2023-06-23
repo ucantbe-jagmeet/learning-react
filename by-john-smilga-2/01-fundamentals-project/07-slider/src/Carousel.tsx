d45a9797c2ffef148cb8180df297b3c4205c841a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-23 17:44:17 +0530

    slider done

--- by-john-smilga-2/01-fundamentals-project/07-slider/src/Carousel.tsx
+++ by-john-smilga-2/01-fundamentals-project/07-slider/src/Carousel.tsx
@@ -4,17 +4,31 @@ import { FaQuoteRight} from "react-icons/fa"
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
 
 const Carousel:React.FC = () => {
-    const [people, setPeople] = useState(shortList)
+    const [people, setPeople] = useState(list)
+    const [currentPerson, setCurrentPerson] = useState(0)
 
-    const prevSlide = () =>{}
-    const nextSlide = () =>{}
+    const prevSlide = () =>{
+        setCurrentPerson( (oldPerson) => {
+            const result = ( oldPerson - 1 + people.length) % people.length
+            return result
+        })
+    }
+    const nextSlide = () =>{
+        setCurrentPerson( (oldPerson) => {
+            const result = ( oldPerson + 1) % people.length
+            return result
+        })
+    }
 
     return (
         <section className='slider-container'>
             {
-                people.map((person)=>{
+                people.map((person, personIndex)=>{
                     const { id , image, name, title, quote}= person
-                    return <article className='slide' key={id}>
+                    return <article className='slide next-slide' key={id} style={{transform: `translateX(${100 * (personIndex - currentPerson)}%)`, 
+                        opacity:personIndex === currentPerson ? 1:0,
+                        visibility:personIndex === currentPerson ? 'visible':'hidden',
+                    }}>
                             <img src={image} alt={name} className='person-img' />
                             <h5 className='name'>{name}</h5>
                             <p className='title'>{title}</p>

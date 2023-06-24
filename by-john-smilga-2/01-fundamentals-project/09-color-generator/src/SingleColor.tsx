fba067ac7f1776f62ed3cf860bdc75672cf68f18
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-24 15:47:10 +0530

    copy to clipboard functionality added

--- by-john-smilga-2/01-fundamentals-project/09-color-generator/src/SingleColor.tsx
+++ by-john-smilga-2/01-fundamentals-project/09-color-generator/src/SingleColor.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 interface IColorsObjects{
     rgb: number[];
    alpha: number;
@@ -14,13 +16,55 @@ interface IColorsObjects{
 
 const SingleColor:React.FC<IColorsArray> = ({ color, index}) => {
     
-  return (
-    <article className={ index > 10 ? 'color color-light':'color'} style={{background: `#${color.hex}`}}>
-        <p className='percent-value'>{color.weight}%</p>
-        <p className='color-value'>{color.hex}%</p>
+    const saveToClipboard = async ()=>{
+        if(navigator.clipboard){
+            try {
+                await navigator.clipboard.writeText(`#${color.hex}`)
+                toast(' Color copied to clipboard', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+            } catch (error) {
+                toast.error('Failed to copy to clipboard', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+            }
+        }
+        else{
+            throw new Error('Clipboard Access not available')
+        }
+    }
 
-    </article>
-  )
+    return (
+        <article className={ index > 10 ? 'color color-light':'color'} style={{background: `#${color.hex}`}} onClick={saveToClipboard}>
+            <ToastContainer
+                    position="top-right"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                    />
+            <p className='percent-value'>{color.weight}%</p>
+            <p className='color-value'>{color.hex}%</p>
+
+        </article>
+    )
 }
 
 export default SingleColor

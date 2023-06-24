2f76a02654c1a51a2dc641f7014b7c795fc53a9d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-24 13:58:33 +0530

    color generator initial commit

---
+++ by-john-smilga-2/01-fundamentals-project/09-color-generator/src/App.tsx
@@ -0,0 +1,14 @@
+import ColorList from "./ColorList"
+import Form from "./Form"
+
+function App() {
+
+  return (
+    <main>
+        <Form/> 
+        <ColorList/>
+    </main>
+  )
+}
+
+export default App
---
+++ by-john-smilga-2/01-fundamentals-project/09-color-generator/src/Form.tsx
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+const Form:React.FC = () => {
+    const [color, setColor] = useState('')
+    const handleSubmit = (e) =>{ }
+  return (
+    <section className='container'>
+        <h4>Color generator</h4>
+        <form className='color-form' onSubmit={handleSubmit}>
+            <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder='#f15025'/>
+            <button className='btn' type='submit' style={{background: color}}>Submit</button>
+        </form>
+
+    </section>
+  )
+}
+
+export default Form

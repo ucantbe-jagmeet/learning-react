16799808a63165b09a3fcce5af8140c72e7b7f4a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-24 14:03:19 +0530

    html color input

--- by-john-smilga-2/01-fundamentals-project/09-color-generator/src/Form.tsx
+++ by-john-smilga-2/01-fundamentals-project/09-color-generator/src/Form.tsx
@@ -2,11 +2,13 @@ import React,{useState} from 'react'
 
 const Form:React.FC = () => {
     const [color, setColor] = useState('')
-    const handleSubmit = (e) =>{ }
+    const handleSubmit = (e) =>{ console.log(e);
+    }
   return (
     <section className='container'>
         <h4>Color generator</h4>
         <form className='color-form' onSubmit={handleSubmit}>
+            <input type="color" value={color} onChange={(e)=> setColor(e.target.value)}/>
             <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder='#f15025'/>
             <button className='btn' type='submit' style={{background: color}}>Submit</button>
         </form>

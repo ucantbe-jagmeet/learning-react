fa6d770c14002d1bb8799b021d15343562e08b35
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-26 20:47:31 +0530

    navbar completed

--- by-john-smilga-2/01-fundamentals-project/11-navbar/src/Navbar.tsx
+++ by-john-smilga-2/01-fundamentals-project/11-navbar/src/Navbar.tsx
@@ -43,6 +43,17 @@ const Navbar: React.FC = () => {
             })}
           </ul>
         </div>
+
+        <div className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </div>
       </div>
     </nav>
   );

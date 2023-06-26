dc6c245d4a2b35d12ff65decf56438861cc23404
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-26 17:52:45 +0530

    navbar structure done

--- by-john-smilga-2/01-fundamentals-project/11-navbar/src/App.tsx
+++ by-john-smilga-2/01-fundamentals-project/11-navbar/src/App.tsx
@@ -1,11 +1,11 @@
+import Navbar from "./Navbar";
 
 function App() {
-
   return (
-    <div>
-        Navbar bolte
-    </div>
-  )
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ by-john-smilga-2/01-fundamentals-project/11-navbar/src/Navbar.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+
+const Navbar: React.FC = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="logo" />
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

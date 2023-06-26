3f2b2cea01e51f454f50a58c7344c2f4a7a85b99
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-26 18:54:37 +0530

    dynamic setup

--- by-john-smilga-2/01-fundamentals-project/11-navbar/src/Navbar.tsx
+++ by-john-smilga-2/01-fundamentals-project/11-navbar/src/Navbar.tsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar: React.FC = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef<HTMLDivElement | null>(null);
+  const linksRef = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    const linkHeight = linksRef.current?.getBoundingClientRect().height;
+    if (linkHeight) {
+      const height = showLinks ? `${linkHeight}px` : "0px";
+      linksContainerRef.current?.style.setProperty("height", height);
+    }
+  }, [showLinks]);
 
   return (
     <nav>
@@ -13,25 +23,26 @@ const Navbar: React.FC = () => {
           <img src={logo} alt="logo" className="logo" />
           <button
             className="nav-toggle"
-            onClick={() => setShowLinks(!showLinks)}
+            onClick={() => {
+              setShowLinks(!showLinks);
+            }}
           >
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );

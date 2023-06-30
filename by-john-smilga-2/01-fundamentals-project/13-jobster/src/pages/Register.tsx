7ad80029078ab80e97c085639ea0cd0acd67f1bb
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-06-30 16:28:46 +0530

    register page setup

--- by-john-smilga-2/01-fundamentals-project/13-jobster/src/pages/Register.tsx
+++ by-john-smilga-2/01-fundamentals-project/13-jobster/src/pages/Register.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register: React.FC = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>login</h3>
+        {/* Name Field */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            className="form-input"
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;

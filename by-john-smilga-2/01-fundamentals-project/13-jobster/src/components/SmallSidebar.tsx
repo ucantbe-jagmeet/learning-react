8611d6b9f4c9586d76fd9bc8b038aeac536b831b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-02 14:46:44 +0530

    small sidebar setup

--- by-john-smilga-2/01-fundamentals-project/13-jobster/src/components/SmallSidebar.tsx
+++ by-john-smilga-2/01-fundamentals-project/13-jobster/src/components/SmallSidebar.tsx
@@ -1,11 +1,25 @@
-import Wrapper from "../assets/wrappers/SmallSidebar";
 import React from "react";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useAppDispatch, useAppSelector } from "../store";
 
 const SmallSidebar: React.FC = () => {
   return (
     <>
       <Wrapper>
-        <h2>smallSidebar</h2>
+        <div className="sidebar-container show-sidebar">
+          <div className="content">
+            <button className="close-btn" onClick={() => console.log("toggle")}>
+              <FaTimes />
+            </button>
+            <header>
+              <Logo />
+            </header>
+            <div className="nav-links">Navlinks</div>
+          </div>
+        </div>
       </Wrapper>
     </>
   );

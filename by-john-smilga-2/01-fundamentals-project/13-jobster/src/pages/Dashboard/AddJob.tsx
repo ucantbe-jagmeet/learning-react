9de852442676c818b8883f0f5a5605c674e584e2
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-03 22:03:26 +0530

    form structure

--- by-john-smilga-2/01-fundamentals-project/13-jobster/src/pages/Dashboard/AddJob.tsx
+++ by-john-smilga-2/01-fundamentals-project/13-jobster/src/pages/Dashboard/AddJob.tsx
@@ -1,7 +1,93 @@
 import React from "react";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { useAppSelector } from "../../store";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { FormRow } from "../../components";
 
 const AddJob: React.FC = () => {
-  return <div>AddJob</div>;
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobTypeOptions,
+    jobType,
+    statusOptions,
+    status,
+    isEditing,
+    editJobId,
+  } = useAppSelector((store) => store.job);
+  const dispatch = useDispatch();
+
+  console.log(isEditing);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      toast.error("Please Fill Out All Fields");
+      return;
+    }
+  };
+
+  const handleJobInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+  };
+
+  return (
+    <Wrapper>
+      <form className="form" onSubmit={handleSubmit}>
+        <h3>{isEditing ? "edit job" : "add job"}</h3>
+        <div className="form-center">
+          {/* position */}
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+            labelText="Position"
+          />
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+            labelText="Company"
+          />
+          {/* location */}
+          <FormRow
+            type="text"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+            labelText="Job Location"
+          />
+          {/* position */}
+
+          <div className="btn-container">
+            <button
+              type="button"
+              className="btn btn-block clear-btn"
+              onClick={() => console.log("clear values")}
+            >
+              clear
+            </button>
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              disabled={isLoading}
+            >
+              {isLoading ? "Please wait..." : "Submit"}
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
